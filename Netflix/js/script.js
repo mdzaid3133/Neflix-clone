eceb0cb3123e1eb95f82b93e731398849d8d1fe3
md zaid <130956080+zaidcoder3133@users.noreply.github.com>
2023-10-10 19:22:29 +0530

    Add files via upload

---
+++ Netflix/js/script.js
@@ -0,0 +1,26 @@
+// switch home page to login page logic
+const signInBtn = document.querySelector('#singInBtn')
+
+signInBtn.addEventListener('click', () => {
+    window.location.href = "LogIn.html";
+})
+
+// open and close FAQ logic
+const allFaqBtn = document.querySelectorAll('.faqBtn');
+
+allFaqBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const nextSibling = btn.nextElementSibling;
+        const isAnswerVisible = nextSibling.classList.contains('show');
+
+        if (isAnswerVisible) {
+            btn.nextElementSibling.classList.remove('show');
+        } else {
+            allFaqBtn.forEach((btn) => {
+                btn.nextElementSibling.classList.remove('show');
+            });
+            nextSibling.classList.add('show');
+        }
+    });
+});
+
